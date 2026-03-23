7e4ee22d3749482ccf922dbacedc53e87cd5f1df
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-03-23 18:11:26 -0300

    F019 a F021 - Componente input, área do Content e Componente Filter

--- comprar/src/app/Home/index.tsx
+++ comprar/src/app/Home/index.tsx
@@ -1,13 +1,26 @@
 import { View, Image} from 'react-native';
-import { Button } from '@/components/Button'
+
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+
 import { styles } from "./styles";
+import { FilterStatus } from '@/types/FilterStatus';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
       
-      <Button title="Entrar"/>
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar"/>
+      </View>
+
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
--- comprar/src/components/Button/index.tsx
+++ comprar/src/components/Button/index.tsx
@@ -7,7 +7,7 @@ type Props = TouchableOpacityProps & {
 
 export function Button({ title, ...rest }: Props) {
     return(
-        //O ...rest coloca todas as outras propriedades de forma implícita
+        //O {...rest} coloca todas as outras propriedades de forma implícita
         <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}> 
             <Text style={styles.title}>{title}</Text>
         </TouchableOpacity>
