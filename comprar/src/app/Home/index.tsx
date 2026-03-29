f8dd823c39c3479247f82b21264ea943194b7a06
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-03-29 10:45:45 -0300

    F022 a F025 - Ícones, Componente StatusIcon, ViewHeader da Content e Componente Item

--- comprar/src/app/Home/index.tsx
+++ comprar/src/app/Home/index.tsx
@@ -1,25 +1,41 @@
-import { View, Image} from 'react-native';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
 
-import { Button } from '@/components/Button';
+import { Item } from '@/components/Item';
 import { Input } from '@/components/Input';
 import { Filter } from '@/components/Filter';
+import { Button } from '@/components/Button';
 
 import { styles } from "./styles";
 import { FilterStatus } from '@/types/FilterStatus';
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
-      
+
       <View style={styles.form}>
         <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar"/>
+        <Button title="Entrar" />
       </View>
 
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive />
-        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Item 
+          data={{status: FilterStatus.DONE, description: "Café"}}
+          onStatus={() => console.log("mudar o Status")}
+          onRemove={() => console.log("remover")}
+        />
       </View>
     </View>
   );
