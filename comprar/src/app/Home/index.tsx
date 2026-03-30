ccd82d9e97750d98d48f5010c900099e00abc1c7
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-03-29 21:20:39 -0300

    F026 a F034 - ScrollView e FlatList, Lista de Itens, Estado x Variável, useState, Estado do Input, Dados in-memory e Explicação Persistir Dados

--- comprar/src/app/Home/index.tsx
+++ comprar/src/app/Home/index.tsx
@@ -1,4 +1,5 @@
-import { View, Image, TouchableOpacity, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Image, TouchableOpacity, Text, FlatList, Alert } from 'react-native';
 
 import { Item } from '@/components/Item';
 import { Input } from '@/components/Input';
@@ -11,19 +12,46 @@ import { FilterStatus } from '@/types/FilterStatus';
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+  const [items, setItems] = useState<any>([]);
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.");
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING
+    }
+
+    // setItems((prevState) => [...prevState, newItem]); -> Dados in-memory
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+        <Input
+          placeholder="O que você precisa comprar?"
+          onChangeText={setDescription}
+        />
+
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.clearButton}>
@@ -31,10 +59,33 @@ export function Home() {
           </TouchableOpacity>
         </View>
 
-        <Item 
-          data={{status: FilterStatus.DONE, description: "Café"}}
-          onStatus={() => console.log("mudar o Status")}
-          onRemove={() => console.log("remover")}
+        {/* Usando ScrollView */}
+        {/* <ScrollView>
+          {ITEMS.map((value) => (
+            <Item
+              key={value}
+              data={{ status: FilterStatus.DONE, description: String(value) }}
+              onStatus={() => console.log("mudar o Status")}
+              onRemove={() => console.log("remover")}
+            />
+          ))}
+        </ScrollView> */}
+
+        {/* Usando FlatList */}
+        <FlatList
+          data={items}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item}
+              onStatus={() => console.log("mudar o Status")}
+              onRemove={() => console.log("remover")}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty} >Nenhum item aqui.</Text>}
         />
       </View>
     </View>
--- comprar/src/components/Filter/index.tsx
+++ comprar/src/components/Filter/index.tsx
@@ -13,6 +13,7 @@ export function Filter({ status, isActive, ...rest }: Props) {
     return (
         <TouchableOpacity
             style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
+            activeOpacity={0.8}
             {...rest}>
 
             <StatusIcon status={status} />
